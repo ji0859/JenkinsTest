4455f9e460d80e5fb621696d76a07fb5723f2379
Eunji Lee <ji0859@kookmin.ac.kr>
2021-05-13 17:03:11 +0900

    test

--- test0859/test3.c
+++ test0859/test3.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%d", &divisor);
 
     // Computes quotient
-    quotient = dividend / 0;
+    quotient = dividend / division;
 
     // Computes remainder
     remainder = dividend % 0;
